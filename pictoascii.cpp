481eae43023a738781711eb36cc8c7d05b950620
Sahil <51375919+sa1123@users.noreply.github.com>
2024-08-01 13:36:34 +0530

    minor fixes

--- pictoascii.cpp
+++ pictoascii.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <ctime>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
@@ -8,27 +7,36 @@
 int main() {
     int width, height, bpp;
 
-    uint8_t* rgb_image = stbi_load("t5.jpg", &width, &height, &bpp, 3);
+    char const* img_f_name = "r1.png";
 
-    //   ofstream MyFile("filename.txt");
+    uint8_t* rgb_image = stbi_load(img_f_name, &width, &height, &bpp, 3);
 
-    for(uint8_t* p = rgb_image, wc = 1; p <= rgb_image + (width * height * bpp); p+=6, wc+=2){
+    // char* output_f_name = img_f_name;
+    // std::strcat(output_f_name, ".txt");
+
+    std::ofstream MyFile("output.txt");
+
+    for(uint8_t* p = rgb_image, wc = 1; p <= rgb_image + (width * height * bpp); p+=3, wc+=1){
         int curr_pix_val = *p + *(p + 1) + *(p + 2);
         if(curr_pix_val < 80){
+            MyFile<<1;
             std::cout<<1;
         }else if(curr_pix_val >= 80 && curr_pix_val < 180){
+            MyFile<<0;
             std::cout<<0;
         }else{
+            MyFile<<" ";
             std::cout<<" ";
         }
         
         if(width == wc){
             std::cout<<"\n";
+            MyFile<<"\n";
             wc = 0;
         }
     }
 
-    // std::cout<<(*rgb_image + *(rgb_image + 1) + *(rgb_image + 2))/3;
+    MyFile.close();
 
     return 0;
 }
